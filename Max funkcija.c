9e1d8b7a74b9e5d57682847e5425eb69a6d82e51
cukelli <95221054+cukelli@users.noreply.github.com>
2021-12-15 23:07:44 +0100

    Add files via upload

---
+++ Max funkcija.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int num1,int num2){
+    int result;
+    if(num1 > num2){
+            result = num1;
+    }else{
+        result = num2;
+    }
+    return result;
+
+    }
+
+int main(){
+printf("Result is %d",max(3,10));
+
+return 0;
+}
+
+
